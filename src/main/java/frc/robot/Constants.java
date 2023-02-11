5056e3d945f26aaf5117bd39cff79ff59f023fe2
Hazel Belmont <hparkerbelmont@gmail.com>
2023-02-10 19:31:42 -0500

    create arm and add constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,12 @@
 package frc.robot;
 
+import org.frc5587.lib.subsystems.PivotingArmBase.PivotingArmConstants;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
  * numerical or boolean
@@ -12,4 +19,20 @@ package frc.robot;
  * wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final class ArmConstants {
+        public static final int motorPort = 15;
+        public static final double gearing = 3; // TODO: Calculate
+        public static final double[] softLimits = {0, Units.degreesToRadians(100)};
+        public static final int zeroOffset = 0; // TODO: Find
+        public static final int encoderCPR = 42; // Change to 2048 if using Falcon
+        public static final int[] limitSwitchPorts = {0};
+        public static final boolean[] switchInverted = {false};
+        public static final TrapezoidProfile.Constraints pidConstraints = new TrapezoidProfile.Constraints(1, 0.2); // TODO: Verify
+        public static final ProfiledPIDController armPIDController = new ProfiledPIDController(1, 0, 0, pidConstraints); // TODO: Characterize
+        public static final ArmFeedforward armFF = new ArmFeedforward(0, 0, 0); // TODO: Characterize
+
+        public static final PivotingArmConstants armConstants = new PivotingArmConstants(
+                gearing, softLimits, zeroOffset, encoderCPR, limitSwitchPorts, switchInverted, armPIDController, armFF);
+    }
+}
